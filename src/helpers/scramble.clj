808a0ee8aeadc70fa54568c1df0582d265760595
Mehrnoosh Rigi <>
2022-07-13 23:51:56 +0200

    scrambler helper

--- src/helpers/scramble.clj
+++ src/helpers/scramble.clj
@@ -2,36 +2,17 @@
 (require '[clojure.string :as str])
 
 (defn isScramble [str1, str2]
-  (println str1, str2)
-  (str/includes? str1 str2)
-  )
-(defn sort-str [s]
-  (-> (str (sort s))
-      (str/split-lines))
+  (def r (apply str (distinct (filter (into #{} str1) str2))))
+  (.equals str2, r) 
   )
 
 (defn take-input [input]
    (println "Please input the" input "string:")
   (def s (read-line))
-  (def sorted-input (sort-str s))
-  (println "sorted input", (first(sort-str s)))
-  sorted-input)
+  (apply str (sort s)) 
+  )
 
 (def first-str (take-input "first"))
 (def second-str (take-input "second"))
 (def result (isScramble first-str second-str))
 (prn "result" result)
-
-;; (-> sort-str
-;;     ;;  (str/split-lines)
-;;      str/split-lines
-;;     ;;  (sort)
-;;      sort
-;;     ;;  (str)
-;;      (println "here")
-;;      str
-;;     ;;  #'
-;;     ;;  (prn "here")
-;;     ;;  (str/join "\\ ")
-;;     ;;  (prn "here2")
-;;   )
