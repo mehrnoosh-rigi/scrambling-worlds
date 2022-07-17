b22a8a8c847f06b7d9317a1812b0f11d4aacc774
Mehrnoosh Rigi <>
2022-07-17 11:50:46 +0200

    scrambler helper

--- src/helpers/scramble.clj
+++ src/helpers/scramble.clj
@@ -3,16 +3,18 @@
 
 (defn isScramble [str1, str2]
   (def r (apply str (distinct (filter (into #{} str1) str2))))
-  (.equals str2, r) 
-  )
+  (.equals str2, r))
 
 (defn take-input [input]
-   (println "Please input the" input "string:")
+  (println "Please input the" input "string:")
   (def s (read-line))
-  (apply str (sort s)) 
-  )
+  (apply str (sort s)))
 
-(def first-str (take-input "first"))
+(let [first-str (take-input "first")]
+  #(Character/isUpperCase %) first-str
+(println ">>>"  #(Character/isUpperCase %) first-str))
 (def second-str (take-input "second"))
-(def result (isScramble first-str second-str))
-(prn "result" result)
+;; (def result (isScramble first-str second-str))
+;; (println "result" result)
+
+;; #(Character/isUpperCase %) str
